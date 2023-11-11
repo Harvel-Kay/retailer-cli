44bd43f2bf29a980763b468cbff7d3abaab3a9fe
Harvel <harvelkay1@gmail.com>
2023-11-11 16:19:13 +0300

    Set the Actual online server url as default api-url

--- src/services/http.ts
+++ src/services/http.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import Store from "../enums/storage";
 import store from "./storage";
 
-axios.defaults.baseURL = "http://localhost:2200/retail";
-// axios.defaults.baseURL = "https://retailer-api.osc-fr1.scalingo.io/retail";
+// axios.defaults.baseURL = "http://localhost:2200/retail";
+axios.defaults.baseURL = "https://retailer-api.osc-fr1.scalingo.io/retail";
 axios.defaults.headers[Store.Auth_id] = store.getToken();
 
 const http = axios;
